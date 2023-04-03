48c11c781e1513a49504f25c8072a4e684ce7a91
Vikramaditya Singh <59855919+Ghat0tkach@users.noreply.github.com>
2023-04-03 22:47:20 +0530

    Add files via upload

---
+++ A2Z-DSA/Arrays/Easy/RotateArray.java
@@ -0,0 +1,30 @@
+package Easy;
+import java.util.Arrays;
+import java.util.Scanner;
+public class RotateArray {
+    public static void main(String[] args) {
+    Scanner in=new Scanner(System.in);
+
+        System.out.println("Enter Size of Array");
+        int k=3;
+        int[] nums={1,2,3,4,5,6,7};
+        k=k%nums.length;
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+        System.out.println(Arrays.toString(nums));
+
+
+
+        }
+    static void reverse(int[] nums,int start,int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+
+        }
+    }
+}
