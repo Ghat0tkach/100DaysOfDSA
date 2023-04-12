ff5550d89fd4feb43bfadf7c83e6fe9a3c4a0566
Vikramaditya Singh <awesomevikram3@gmail.com>
2023-04-12 23:34:51 +0530

    added more questions

--- A2Z-DSA/Arrays/Medium/LongestConsecutiveSubsequence.java
+++ A2Z-DSA/Arrays/Medium/LongestConsecutiveSubsequence.java
@@ -1,6 +1,43 @@
 package Medium;
 
 public class LongestConsecutiveSubsequence {
+    /*
+       import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // Create a hash set to store all the unique elements in nums
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        // Iterate over each element in nums
+        for (int num : nums) {
+            // If num is the first element of a sequence, start counting the length of the sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // Keep incrementing currentNum until we find the end of the sequence
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                // Update the longest streak if necessary
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
+
+    * */
+
     public static void main(String[] args) {
         int[] arr={1,3,2,4,101,230};
         int largest=1;
@@ -25,4 +62,5 @@ public class LongestConsecutiveSubsequence {
         }
         return false;
     }
+
 }
---
+++ A2Z-DSA/Arrays/Medium/SetMatrixZero.java
@@ -0,0 +1,35 @@
+package Medium;
+
+public class SetMatrixZero {
+    public static void main(String[] args) {
+
+    }
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length; // number of rows
+        int n = matrix[0].length; // number of columns
+
+        // Step 1: Create boolean arrays to track which rows and columns to set to zero
+        boolean[] rows = new boolean[m];
+        boolean[] cols = new boolean[n];
+
+        // Step 2: Iterate through the matrix and set rows and columns to true if a 0 is found
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        // Step 3: Iterate through the matrix again and set elements to zero if their row or column is marked
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+}
