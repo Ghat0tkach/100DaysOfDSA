4d4e3f00a5bc03a79cae6e3b99f9fc04179b711f
Vikramaditya Singh <awesomevikram3@gmail.com>
2023-04-18 23:46:44 +0530

    commit

---
+++ A2Z-DSA/Arrays/BinarySearchEasy/Searchin2dMatrix.java
@@ -0,0 +1,43 @@
+package BinarySearchEasy;
+
+public class Searchin2dMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+        int target = 3;
+        boolean result = searchMatrix(matrix, target);
+        System.out.println(result);
+    }
+
+
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) {
+            return false;
+        }
+        int n = matrix[0].length;
+        if (n == 0) {
+            return false;
+        }
+
+        int low = 0;
+        int high = m * n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int row = mid / n;
+            int col = mid % n;
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+
--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int count=0;
+        for(int i=0;i<n;i++){
+
+            if( OneZero(i)){
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+    static boolean OneZero(int n){
+        int count=0;
+        if(n<10){
+            return true;
+        }
+        else{
+            while(n!=0){
+                if((n%10)!=0){
+                    count++;
+                }
+                n=n/10;
+            }
+            if(count==1){
+                return true;
+            }
+        }
+
+
+        return false;
     }
 }
