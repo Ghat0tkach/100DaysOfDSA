f3977b9db78b8d385330429751c46deb9c23734b
Vikram <awesomevikram3@gmail.com>
2023-04-25 23:50:54 +0530

    ok

--- LeetCode/src/Arrays/DaysofCode.java
+++ LeetCode/src/Arrays/DaysofCode.java
@@ -4,26 +4,37 @@ import java.util.Arrays;
 import java.util.Scanner;
 public class DaysofCode {
     public static void main (String[] args) {
-        // Your code here
-        Scanner in=new Scanner(System.in);
-        int k =in.nextInt();
-        int[] index=new int[k];
-        int count = 0;
 
-        for (int i = 1; ; i++) {
-            if (i % 3 != 0 && i % 10 != 3) {
-                count++;
-                if (count == k) {
-                    System.out.println(i);
-                    break;
-                }
+            // Your code here
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int x = in.nextInt();
+            int b = 2 * n;
+            int[] arr = new int[b];
+            for (int i = 0; i < b; i++) {
+                arr[i] = in.nextInt();
             }
+            Arrays.sort(arr);
+            int count = 0;
+            int start = 0;
+            int end = b-1;
+            while (start < end) {
+                if (arr[end] - arr[start] < x) {
+                    count++;
+
+                }
+                start++;
+                end--;
+            }
+            if (count != 0) {
+                System.out.println("No");
+            } else {
+
+                System.out.println("Yes");
+
+            }
+
         }
 
 
     }
-
-    }
-
-
-
